12b8408e87d61879d9442dd453a2afad0e43ec24
meffersino <michaelpjeffrey@hotmail.co.uk>
2019-01-13 11:46:04 +0000

    added - read and add up contents of a file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,55 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
 
 using namespace std;
 
 
-string helloName(string input) {
-    return "hello " + input;
+list<int> * processFile(list<int> * output, ifstream * infile) {
+    cout << "1" << endl;
+    
+    string line;
+    while(getline(*infile, line))
+    {
+        cout << "2" << endl;
+        istringstream iss(line);
+        cout << "3" << endl;
+        int value;
+        if(iss >> value)
+        {
+            cout << "4" << endl;
+            cout << "value: " << value << endl;
+            output->push_back(value);
+            cout << "5" << endl;
+        }
+    }
 
+
+    return output;
+}
+
+int addValuesFromList(list<int> * list, int startValue)
+{
+    int result = startValue;
+    for(auto const& i : *list)
+    {
+        result += i;
+    }
+
+
+    return result;
 }
 
 int main() {
-    cout << helloName("world") << endl;
+    ifstream infile("input.txt");
+    list<int> * processedList = new list<int>;
+    processedList = processFile(processedList, &infile);
+    int startValue = 0;
+    int result = addValuesFromList(processedList, startValue);
+
+    cout << "result: " << result << endl;
     return 0;
 
 }
